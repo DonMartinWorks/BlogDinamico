69721c94ebc2ff5895a7d7da071fea0e62491062
DonMartinWorks <donmartinworks@gmail.com>
2023-02-20 15:58:10 -0300

    Vista base

--- routes/web.php
+++ routes/web.php
@@ -18,6 +18,10 @@ Route::get('/', function () {
     return view('welcome');
 });
 
+Route::get('/base', function () {
+    return view('base');
+});
+
 Route::get('/dashboard', function () {
     return view('dashboard');
 })->middleware(['auth', 'verified'])->name('dashboard');
