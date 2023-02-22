c7e9ab1f6a6b6fd73d067a1d2a46be1f96bc6e3f
DonMartinWorks <donmartinworks@gmail.com>
2023-02-22 15:59:16 -0300

    Cargando los items del menu al componente

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -3,6 +3,7 @@
 namespace Database\Seeders;
 
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use App\Models\Navitem;
 use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder
@@ -16,9 +17,25 @@ class DatabaseSeeder extends Seeder
     {
         // \App\Models\User::factory(10)->create();
 
-        // \App\Models\User::factory()->create([
-        //     'name' => 'Test User',
-        //     'email' => 'test@example.com',
-        // ]);
+        \App\Models\User::factory()->create([
+            'name' => 'Usuario Administrador',
+            'email' => 'admin@admin.com',
+            'password' => bcrypt('Admin123'),
+        ]);
+
+        Navitem::factory()->create([
+            'label' => __('Greetings'),
+            'link' => '#greetings'
+        ]);
+
+        Navitem::factory()->create([
+            'label' => __('Projects'),
+            'link' => '#projects'
+        ]);
+
+        Navitem::factory()->create([
+            'label' => __('Contact Me'),
+            'link' => '#contact'
+        ]);
     }
 }
--- tests/Feature/Navigation/NavigationTest.php
+++ tests/Feature/Navigation/NavigationTest.php
@@ -2,9 +2,12 @@
 
 namespace Tests\Feature\Navigation;
 
-use Illuminate\Foundation\Testing\RefreshDatabase;
-use Illuminate\Foundation\Testing\WithFaker;
 use Tests\TestCase;
+use Livewire\Livewire;
+use App\Models\Navitem;
+use App\Http\Livewire\Navigation\Navigation;
+use Illuminate\Foundation\Testing\WithFaker;
+use Illuminate\Foundation\Testing\RefreshDatabase;
 
 class NavigationTest extends TestCase
 {
@@ -18,5 +21,19 @@ class NavigationTest extends TestCase
     public function navigation_component_can_be_rendered()
     {
         $this->get('/')->assertStatus(200)->assertSeeLivewire('navigation.navigation');
+    }
+
+    /**
+     * Esta prueba se encarga de cargar y renderizar los enlaces a la vista.
+     *
+     * @test
+     */
+    public function component_can_load_items_navigation()
+    {
+        $items = Navitem::factory(3)->create();
+
+        Livewire::test(Navigation::class)
+        ->assertSee($items->first()->label)
+        ->assertSee($items->first()->link);
     }
 }
