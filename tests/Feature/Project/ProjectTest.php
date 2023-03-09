38a57af229619cabe83547d5cf98fbdb0ad08b43
DonMartinWorks <donmartinworks@gmail.com>
2023-03-09 15:12:37 -0300

    Tests para reglas de validación para PROJECT

--- tests/Feature/Project/ProjectTest.php
+++ tests/Feature/Project/ProjectTest.php
@@ -199,4 +199,173 @@ class ProjectTest extends TestCase
         $this->assertDatabaseMissing('projects', ['id' => $project->id]);
         Storage::disk('projects')->assertMissing($project->image);
     }
+
+
+    /**
+     * Prueba encargada de corroborar que solo el nombre en el project es requerido.
+     *
+     * @test
+     */
+    public function name_is_required()
+    {
+        $user = User::factory()->create();
+
+        Livewire::actingAs($user)->test(Project::class)
+            ->set('currentProject.name', '')
+            ->call('save')
+            ->assertHasErrors(['currentProject.name' => 'required']);
+    }
+
+
+
+    /**
+     * Prueba encargada de corroborar que solo el nombre no supere los 100 caracteres.
+     *
+     * @test
+     */
+    public function name_must_have_a_maximum_of_one_hundred_characters()
+    {
+        $user = User::factory()->create();
+
+        Livewire::actingAs($user)->test(Project::class)
+            ->set('currentProject.name', 'vitae. Aliquid necessitatibus asperiores atque quidem laborum cum optio veritatis debitis repellat, delectus doloremque')
+            ->call('save')
+            ->assertHasErrors(['currentProject.name' => 'max']);
+    }
+
+    /**
+     * Prueba encargada de corroborar que solo la descripcion en el project es requerido.
+     *
+     * @test
+     */
+    public function description_is_required()
+    {
+        $user = User::factory()->create();
+
+        Livewire::actingAs($user)->test(Project::class)
+            ->set('currentProject.description', '')
+            ->call('save')
+            ->assertHasErrors(['currentProject.description' => 'required']);
+    }
+
+    /**
+     * Prueba encargada de corroborar que solo la descricion no supere los 450 caracteres.
+     *
+     * @test
+     */
+    public function description_must_have_a_maximum_of_four_hundred_fifty_characters()
+    {
+        $user = User::factory()->create();
+
+        Livewire::actingAs($user)->test(Project::class)
+            ->set('currentProject.description', 'Lorem, ipsum dolor sit amet consectetur adipisicing elit. Fugiat quia sit voluptas dicta accusantium officiis hic! Dolor voluptatibus iusto totam. Iste inventore quas saepe consequatur amet fuga cupiditate minus reiciendis.
+            Totam quaerat qui exercitationem cumque quae obcaecati, sed aliquid consectetur, nisi suscipit amet quod eveniet deserunt itaque et magnam debitis eligendi expedita, voluptatum iure ipsa sunt rem architecto explicabo. Cum.')
+            ->call('save')
+            ->assertHasErrors(['currentProject.description' => 'max']);
+    }
+
+    /**
+     * Prueba encargada de corroborar que solo certifique la imagen como imagen.
+     *
+     * @test
+     */
+    public function image_file_must_be_a_image()
+    {
+        $user = User::factory()->create();
+
+        Livewire::actingAs($user)->test(Project::class)
+            ->set('imageFile', UploadedFile::fake()->create('myfile.pdf'))
+            ->call('save')
+            ->assertHasErrors(['imageFile' => 'image']);
+    }
+
+    /**
+     * Prueba encargada de corroborar que solo certifique la imagen no supere 1 MB.
+     *
+     * @test
+     */
+    public function image_file_must_be_max_one_megabyte()
+    {
+        $user = User::factory()->create();
+
+        Livewire::actingAs($user)->test(Project::class)
+            ->set('imageFile', UploadedFile::fake()->image('myimage.jpg')->size(1025))
+            ->call('save')
+            ->assertHasErrors(['imageFile' => 'max']);
+    }
+
+    /**
+     * Prueba encargada de corroborar que solo certifique la imagen no supere 1 MB.
+     *
+     * @test
+     */
+    public function video_link_must_be_a_valid_url()
+    {
+        $user = User::factory()->create();
+
+        Livewire::actingAs($user)->test(Project::class)
+            ->set('currentProject.video_link', 'http:/www.google.com')
+            ->call('save')
+            ->assertHasErrors(['currentProject.video_link' => 'url']);
+    }
+
+    /**
+     * Prueba encargada de corroborar que el link de youtube sea un link valido.
+     *
+     * @test
+     */
+    public function video_link_must_match_with_regex()
+    {
+        $user = User::factory()->create();
+
+        Livewire::actingAs($user)->test(Project::class)
+            ->set('currentProject.video_link', 'https://www.youtube.com/')
+            ->call('save')
+            ->assertHasErrors(['currentProject.video_link' => 'regex']);
+    }
+
+    /**
+     * Prueba encargada de corroborar que el url link sea un link valido de una pagina.
+     *
+     * @test
+     */
+    public function url_must_be_a_valid_url()
+    {
+        $user = User::factory()->create();
+
+        Livewire::actingAs($user)->test(Project::class)
+            ->set('currentProject.url', 'http:/www.google.com')
+            ->call('save')
+            ->assertHasErrors(['currentProject.url' => 'url']);
+    }
+
+    /**
+     * Prueba encargada de corroborar que el repo sea un link valido de repositorio.
+     *
+     * @test
+     */
+    public function repo_url_must_be_a_valid_url()
+    {
+        $user = User::factory()->create();
+
+        Livewire::actingAs($user)->test(Project::class)
+            ->set('currentProject.repo_url', 'http:/www.google.com')
+            ->call('save')
+            ->assertHasErrors(['currentProject.repo_url' => 'url']);
+    }
+
+    /**
+     * Prueba encargada de corroborar que el link del repositorio sea un link valido (Github/Gitlab).
+     *
+     * @test
+     */
+    public function repo_url_must_match_with_regex()
+    {
+        $user = User::factory()->create();
+
+        Livewire::actingAs($user)->test(Project::class)
+            ->set('currentProject.repo_url', 'https://nogithub.com/DonMartinWorks/BlogDinamico')
+            ->call('save')
+            ->assertHasErrors(['currentProject.repo_url' => 'regex']);
+    }
 }
